a33281e2fbc6bf6f4daff963bea89d5721f5c209
Keith Dahlby <keith@newbo.co>
2018-10-11 11:00:31 -0500

    Add Socket.io server

--- server.js
+++ server.js
@@ -4,6 +4,33 @@ require('dotenv').config();
 
 const app = express();
 
+// Wire up Socket.io
+const httpServer = require('http').Server(app);
+const realTimeServer = require('socket.io')(httpServer);
+
+var id = 0;
+realTimeServer.on('connection', socket => {
+  console.log('connection', socket.id);
+
+  socket.on('SEND_MESSAGE', message => {
+    console.log('socket event', 'SEND_MESSAGE');
+    socket.emit('RECEIVE_MESSAGE', 'You sent a message!');
+    realTimeServer.emit('RECEIVED_MESSAGE', {
+      ...message,
+      id: ++id,
+      timestamp: new Date(),
+    });
+  });
+});
+
+realTimeServer.on('disconnect', () => {
+  console.log('disconnect', realTimeServer.id);
+});
+
+realTimeServer.on('error', error => {
+  console.error(error);
+});
+
 // Log individual requests
 app.use(morgan('tiny'));
 
@@ -13,6 +40,6 @@ app.use(express.static(`${__dirname}/build`));
 // Anything else gets handled by React
 app.get('*', (req, res) => res.sendFile(`${__dirname}/build/index.html`));
 
-app.listen(process.env.PORT, () => {
+httpServer.listen(process.env.PORT, () => {
   console.log(`Listening on ${process.env.PORT}`);
 });
